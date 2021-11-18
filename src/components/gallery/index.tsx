a02af35386c00e55d539e7c79ff324fa2f47aeee
Given Somdaka <given@binay.co.za>
2021-11-18 22:11:32 +0200

    Added illustration gallery

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -4,20 +4,28 @@ import './styles.scss';
 
 interface IProps {
   images: IGalleryImage[];
+  title: string;
 }
 
 const { Meta } = Card;
 
-const Gallery = ({ images }: IProps) => {
+const Gallery = ({ images, title }: IProps) => {
   return (
-    <div className='gallery'>
-      <Image.PreviewGroup>
-        {images.map((image) => (
-          <Card style={{ width: '100%' }} cover={<Image id={image.id} src={image.url} className='thumbnail' />}>
-            <Meta title='Europe Street beat' description='www.instagram.com' />
-          </Card>
-        ))}
-      </Image.PreviewGroup>
+    <div className='container'>
+      <h3>{title}</h3>
+      <div className='gallery'>
+        <Image.PreviewGroup>
+          {images.map((image) => (
+            <Card
+              bordered={false}
+              cover={
+                <Image id={image.id} src={image.url} className='thumbnail' style={{ height: '320px', width: 'auto' }} />
+              }>
+              <Meta title={image.title} description={image.description} />
+            </Card>
+          ))}
+        </Image.PreviewGroup>
+      </div>
     </div>
   );
 };
